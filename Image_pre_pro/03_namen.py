b4695e1625b1e0f0a3c7dfed8fe9787354d12fc3
KJJ2002 <99194650+KJJ2002@users.noreply.github.com>
2024-01-30 16:35:31 +0100

    Bilder_Benni einsortiert und zugeschnitten

--- Image_pre_pro/03_namen.py
+++ Image_pre_pro/03_namen.py
@@ -11,11 +11,11 @@ anpassen:
   - start_number  -> damit es keine Duplikate gibt bei nächstgrößer Zahl anfangen, die schon vergeben ist
 """
 
-target_folder = '../02_data_crop/train/g'
-images_folder = '../test'
+target_folder = '../02_data_crop/train/z'
+images_folder = '../Bilder_Benni/Z'
 
-prefix = 'g'
-start_number = 29
+prefix = 'z'
+start_number = 67
 
 
 for file in os.listdir(images_folder):
