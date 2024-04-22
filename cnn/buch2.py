bfe35c327775b546f88cb649e4fb29c696c81127
KJJ2002 <99194650+KJJ2002@users.noreply.github.com>
2024-04-22 15:10:38 +0200

    versuch mit resnet

--- cnn/buch2.py
+++ cnn/buch2.py
@@ -2,19 +2,21 @@ import os
 import numpy as np
 from natsort import natsorted
 import matplotlib.pyplot as plt
+import keras
+import tensorflow as tf
 
+from keras.preprocessing import image
+from keras.utils import to_categorical 
+
+from tensorflow.keras import backend as K
+from tensorflow.keras import models
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras import backend as K
-from tensorflow.keras import models
-import keras
-import tensorflow as tf
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.applications.inception_v3 import preprocess_input, decode_predictions
-from keras.preprocessing import image
-from keras.utils import to_categorical 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -24,6 +26,10 @@ session = InteractiveSession(config=config)
 
 
 class Data:
+    """
+    speichert Bilder in array als Graustufenbild
+    speichert Labels zu den Bildern als Namen der Unterordnern
+    """
 
     def __init__(self, images):
         self.images_path = images
@@ -66,6 +72,9 @@ class Data:
 
 
 class Data_preparation:
+    """
+    daten als array, numpy, normalisiert
+    """
 
     def __init__(self):
         self.data_train = Data('cnn/resized_images/train')
@@ -83,11 +92,11 @@ class Data_preparation:
 
 
     def get_data_as_numpy_array(self, train_array_images, train_array_labels, test_array_images, test_array_labels):
-        train_numpy_images = np.array([tensor.numpy() for tensor in train_array_images])
-        train_numpy_labels = np.array([tensor.numpy() for tensor in train_array_labels])
+        train_numpy_images = np.array(train_array_images)
+        train_numpy_labels = np.array(train_array_labels)
 
-        test_numpy_images = np.array([tensor.numpy() for tensor in test_array_images])
-        test_numpy_labels = np.array([tensor.numpy() for tensor in test_array_labels])
+        test_numpy_images = np.array(test_array_images)
+        test_numpy_labels = np.array(test_array_labels)
 
         return train_numpy_images, train_numpy_labels, test_numpy_images, test_numpy_labels
     
@@ -116,24 +125,28 @@ class Data_preparation:
 
 
 class Model:
+    """
+    sequential model
+    """
 
-    def __init__(self, train_images, train_labels, test_images, test_labels):
+    def __init__(self, train_images, train_labels, test_images, test_labels, input_shape):
         self.train_images = train_images
         self.train_labels = train_labels
         self.test_images = test_images
         self.test_labels = test_labels
 
-        self.model = self.initialize()
+        self.model = self.model_architecture()
         self.compiled_model = self.compile()
+        self.input_shape = input_shape
 
 
-    def initialize(self):
+    def model_architecture(self):
         # Netzwerkarchitektur
-        num_classes = 27
         model = Sequential()
+        num_classes = 27
 
             # Detektion
-        model.add(Conv2D(32, kernel_size=(5,5), activation= 'relu', input_shape=(250, 250))) # input shape der images
+        model.add(Conv2D(32, kernel_size=(5,5), activation= 'relu', input_shape=self.input_shape)) # input shape der images
 
             # Conv_Block 2
         model.add(Conv2D(64, kernel_size=(5,5), activation='relu'))
@@ -189,14 +202,31 @@ class Model:
 if __name__ == "__main__":
     data_prep = Data_preparation()
     train_array_images, train_array_labels, test_array_images, test_array_labels = data_prep.get_data_as_array()
+    #print(train_array_labels)
+    unique_classes = np.unique(train_array_labels)
+    num_classes = len(unique_classes)
+    print("Einzigartige Klassen:", unique_classes)
+    print("Anzahl der einzigartigen Klassen:", num_classes)
+
+
     train_numpy_images, train_numpy_labels, test_numpy_images, test_numpy_labels = data_prep.get_data_as_numpy_array(train_array_images, train_array_labels, test_array_images, test_array_labels)
-    
+    print("Shape train_numpy_images: {}".format(train_numpy_images.shape))
+    print("Shape test_numpy_images: {}".format(test_numpy_images.shape))
+    print("Shape train_numpy_labels: {}".format(train_numpy_labels.shape))
+    print("Shape test_numpy_labels: {}".format(test_numpy_labels.shape))
+
     train_normalized_images, test_normalized_images = data_prep.prep_images(train_numpy_images, test_numpy_images)
     train_binary_labels, test_binary_labels = data_prep.prep_label(train_numpy_labels, test_numpy_labels)
 
-    cnn = Model(train_normalized_images, train_binary_labels, test_normalized_images, test_binary_labels)
-    cnn.compile()
-    cnn.train()
-    cnn.evaluate()
-    cnn.save()
+    unique_sums = np.sum(train_binary_labels, axis=0)
+    num_unique_classes = np.count_nonzero(unique_sums)
+    print("Anzahl der einzigartigen Klassen:", num_unique_classes)
+
+    input_shape = train_numpy_images.shape[1:]
+    print(input_shape)
+    #cnn = Model(train_normalized_images, train_binary_labels, test_normalized_images, test_binary_labels, input_shape)
+    #cnn.compile()
+    #cnn.train()
+    #cnn.evaluate()
+    #cnn.save()
 
