641bd5f84890df2a612b0e840d9c2e2e41151a4d
KJJ2002 <99194650+KJJ2002@users.noreply.github.com>
2024-04-22 21:58:35 +0200

    augmentation

--- Image_pre_pro/04_crop.py
+++ Image_pre_pro/04_crop.py
@@ -87,5 +87,5 @@ def Crop_Images_Hands(input_folder):
      except Exception as e:
           print(str(e))
 
-source_path = '../02_data_crop/test/26'
+source_path = './test/'
 Crop_Images_Hands(input_folder=source_path)
